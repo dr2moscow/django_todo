4b6c069f6c585d5dd3c490724b3cc52dcaf8d406
dr2moscow <dr2moscow@gmail.com>
2021-11-01 08:25:36 +0300

    Создал модель БД с уникальным email

--- todo/authors/models.py
+++ todo/authors/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Authors(models.Model):
+   username = models.CharField(max_length=64,blank=False,unique=True)
+   first_name = models.CharField(max_length=64,blank=False)
+   last_name = models.CharField(max_length=64,blank=False)
+   email = models.EmailField(max_length=70,blank=False)
+
